a1b76da7e1688067ef50b4c6cb206455e037937e
MartinNilsen99 <martinjnilsen@icloud.com>
2021-02-02 18:23:17 +0100

    Progress?

--- model/struct_data.py
+++ model/struct_data.py
@@ -1,8 +1,9 @@
 import numpy
 import pandas as pd
 
+
 class struct_data:
-    
+
     def __init__(self, data_f_name, annot_f_name, pose_map):
         self.data_f_name = data_f_name
         self.annot_f_name = annot_f_name
@@ -11,18 +12,20 @@ class struct_data:
         csv = pd.read_csv(self.data_f_name)
         df = pd.DataFrame(csv)
 
-        df_time_offset = df["Timestamp"][0]
-        df["Timestamp"] = df["Timestamp"] - df_time_offset
+        df_time_offset = df[" TimeStamp (s)"][0]
+        df[" TimeStamp (s)"] = df[" TimeStamp (s)"] - df_time_offset
         self.df = df
 
     def get_timestamp_and_pose(self):
         rows = []
         with open(self.annot_f_name, "r") as f:
             lines = f.readlines()
-            offset = float(lines[1].strip().split(";")[1]) # This offset is the duration spent in the video before recording of data begun.
+            # This offset is the duration spent in the video before recording of data begun.
+            offset = float(lines[1].strip().split(";")[1])
             for l in lines[1:]:
                 sep_row = l.strip().split(";")
-                finished_row = [round(float(x)-offset,2) for x in sep_row[1:3]]
+                finished_row = [round(float(x)-offset, 2)
+                                for x in sep_row[1:3]]
                 finished_row.append(self.pose_map[sep_row[3].lower()])
                 rows.append(finished_row)
         return rows
@@ -32,7 +35,7 @@ class struct_data:
         pose_index = 0
         row_index = 0
         drops = 0
-        for stamp in self.df["Timestamp"]:  
+        for stamp in self.df[" TimeStamp (s)"]:
             pose_id = -1
             if stamp <= stamped_poses[-1][1] and stamp >= stamped_poses[0][0]:
                 if stamp > stamped_poses[pose_index][1]:
@@ -53,5 +56,22 @@ class struct_data:
 
         return self.df, df_stamped_poses
 
-    if __name__ == "__main__":
-        pass
+
+if __name__ == "__main__":
+
+    csv_f_name = "data/train_data/data_martin_1_forsok2.csv"
+    annot_f_name = "data/annotation/martin_train_1.txt"
+
+    POSE_MAP = {
+        "rett": 0,
+        "fram": 1,
+        "fram-hoyre": 2,
+        "hoyre": 3,
+        "bak-hoyre": 4,
+        "bak": 5,
+        "bak-venstre": 6,
+        "venstre": 7,
+        "fram-venstre": 8
+    }
+
+    noe = struct_data(csv_f_name, annot_f_name, POSE_MAP)
