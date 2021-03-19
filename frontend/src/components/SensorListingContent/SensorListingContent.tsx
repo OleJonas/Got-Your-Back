da622ba9ba61092236234772714d841ab75fe2de
olejlia <olejlia@stud.ntnu.no>
2021-03-19 12:04:35 +0100

    Some work on a new list component for the sensors

--- frontend/src/components/SensorListingContent/SensorListingContent.tsx
+++ frontend/src/components/SensorListingContent/SensorListingContent.tsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Box, Typography } from "@material-ui/core";
 import { Button } from "../Buttons/Button.component";
+import {SensorList} from "../SensorList/SensorList"
+
+type SensorListProps = {
+	sensorsListed?: any[]
+};
 
 export const SensorListingContent = () => {
 	const classes = useStyles();
 
 	const [rate, setRate] = useState(5);
 
-	return <Box></Box>;
+	return( 
+		<Box>
+			
+		</Box>
+	);
 };
 
 const useStyles = makeStyles({});
