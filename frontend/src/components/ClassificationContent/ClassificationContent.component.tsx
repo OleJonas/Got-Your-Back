2183a08e5e13faaab26b3d3b74a4be6fe33be22a
MartinNilsen99 <martinjnilsen@icloud.com>
2021-03-22 14:19:32 +0100

    classification content

--- frontend/src/components/ClassificationContent/ClassificationContent.component.tsx
+++ frontend/src/components/ClassificationContent/ClassificationContent.component.tsx
@@ -5,6 +5,8 @@ import { Grid, Box, Typography } from "@material-ui/core";
 // Components
 import { Button } from "../Buttons/Button.component";
 import { posture_names } from "../../utils/posture_names";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
 
 type ClassificationProps = {
 	posture: number;
@@ -18,40 +20,43 @@ export const ClassificationBox: React.FC<ClassificationProps> = (props) => {
 
 	const onStartPressed = () => {
 		setStartRecordRequested(true);
-
 		//Send request til server
 	};
 
 	return (
 		<Box className={classes.root}>
 			<Grid className={classes.grid} justify="center" alignItems="center" container item xs={12}>
-				<Grid item xs={12}>
-					<Button func={() => onStartPressed()} disabled={startRecordRequested === true ? true : false}>
-						{startRecordRequested === true ? "Starting up ..." : props.recording === true ? "Stop recording" : "Start recording"}
-					</Button>
-				</Grid>
-
 				<Grid item xs={7}>
-					<Typography variant="subtitle1" color="textPrimary">
-						{props.posture}
-					</Typography>
+					<Box display="flex" justifyContent="center" alignItems="center">
+						<Typography variant="subtitle1" color="textPrimary">
+							{props.posture}
+						</Typography>
+						<Box ml={2}>
+							{props.recording ? <PlayArrowIcon className={classes.recordIcon} /> : <PauseIcon className={classes.recordIcon} />}
+						</Box>
+					</Box>
 					<Typography variant="h2" color="textPrimary">
 						{posture_names[props.posture]}
 					</Typography>
 				</Grid>
 
 				<Grid item xs={5}>
-					<Box display="flex" justifyContent="center" alignItems="center">
+					<Box display="flex" justifyContent="center" alignItems="flex-end">
 						<Typography variant="subtitle2" color="textPrimary">
 							{props.samplingRate}
 						</Typography>
-						<Box ml={1}>
+						<Box ml={1} mb={1}>
 							<Typography variant="h2" color="textPrimary">
 								{"Hz"}
 							</Typography>
 						</Box>
 					</Box>
-					<Button>Change</Button>
+				</Grid>
+
+				<Grid item xs={12}>
+					<Button func={() => onStartPressed()} disabled={startRecordRequested ? true : false}>
+						{startRecordRequested ? "Starting up ..." : props.recording ? "Stop recording" : "Start recording"}
+					</Button>
 				</Grid>
 			</Grid>
 		</Box>
@@ -68,5 +73,9 @@ const useStyles = makeStyles({
 	},
 	btn: {
 		margin: "auto",
+	},
+	recordIcon: {
+		color: "white",
+		fontSize: "50px",
 	},
 });
