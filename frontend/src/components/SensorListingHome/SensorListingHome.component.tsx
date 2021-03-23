51957f78a9215ca23701dc362d5f6dee2abdf3aa
olejlia <olejlia@stud.ntnu.no>
2021-03-23 10:09:01 +0100

    Fixing battery percent

--- frontend/src/components/SensorListingHome/SensorListingHome.component.tsx
+++ frontend/src/components/SensorListingHome/SensorListingHome.component.tsx
@@ -26,28 +26,6 @@ export const SensorListingHome: FC<SensorProps> = (props) => {
 	const [connected, setConnected] = useState<boolean>(false);
 	const classes = useStyles();
 
-	const connect = useCallback(async () => {
-		if (isFetching) return;
-		setIsFetching(true);
-
-		await fetch("http://localhost:5000/setup/connect", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Accept: "application/json",
-			},
-			body: JSON.stringify({
-				handle: props.index,
-			}),
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-				setConnected(true);
-				setIsFetching(false);
-			});
-	}, [isFetching]);
-
 	useEffect(() => {
 		if (!props.clickConnect) return;
 		props.clickConnect(props.index, connected);
@@ -55,7 +33,7 @@ export const SensorListingHome: FC<SensorProps> = (props) => {
 
 	const disconnect = useCallback(async () => {
 		console.log("Disconnected!");
-		/*
+		
 		await fetch("http://localhost:5000/setup/disconnect", {
 			method: "POST",
 			headers: {
@@ -63,7 +41,7 @@ export const SensorListingHome: FC<SensorProps> = (props) => {
 				Accept: "application/json",
 			},
 			body: JSON.stringify({
-				handle: props.index,
+				handles: [props.index],
 			}),
 		})
 			.then((res) => res.json())
@@ -71,35 +49,32 @@ export const SensorListingHome: FC<SensorProps> = (props) => {
 				console.log(data);
 				setConnected(false);
 			});
-        */
 	}, [connected]);
 
-	/*
+	
 	const getBatteryPercent = useCallback(async () => {
-		
+		if(!props.connected) return;
 		await fetch("http://localhost:5000/sensor/battery?id=" + props.index, {
 			method: "GET",
 			headers: {
-				"Content-Type": "application/json",
 				Accept: "application/json",
 			},
 		})
-			.then((res) => {
-				console.log(res.text());
-				//res.json();
-			})
-			.then((data) => {
-				console.log(data);
-				setBatteryPercent(data);
+			.then(res => {
+				res.json()
+				console.log(res)})
+			/*
+			.then(data => {
+				if(data !== undefined) setBatteryPercent(data.battery);
 			});
-		
+			*/
 	}, [batteryPercent]);
 
 	useEffect(() => {
 		if (!props.connected) return;
 		setInterval(getBatteryPercent, 5000);
 	}, []);
-    */
+
 	return (
 		<Grid container className={classes.root}>
 			<Grid container item className={classes.grid} direction="row" justify="center" xs={2}>
@@ -119,7 +94,7 @@ export const SensorListingHome: FC<SensorProps> = (props) => {
 			</Grid>
 			<Grid container item className={classes.grid} direction="row" justify="center" xs={2}>
 				<Typography variant="body1" color="textPrimary">
-					{"3,7%"}
+					{batteryPercent}
 				</Typography>
 			</Grid>
 			<Grid className={classes.grid} container justify="center" item xs={3}>
