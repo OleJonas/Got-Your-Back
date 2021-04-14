77fae4e6306645d53cd80ff022d9b84ef9e33607
MartinNilsen99 <martinjnilsen@icloud.com>
2021-04-14 12:29:13 +0200

    Types

--- scripts/rnn_utils.py
+++ scripts/rnn_utils.py
@@ -1,26 +1,42 @@
-
 from scipy import stats
-import numpy as np 
+import numpy as np
 
-def create_3d_array(array, num_timestamps):
 
+def create_3d_array(array: list, num_timestamps: int):
+    """Generate three-dimensional array for RNN
+
+    Args:
+        array ([float]): Data from sensors to be concatted
+        num_timestamps (int): Number of timestamps to concat into new array
+
+    Returns:
+        [[[float]]]: Three dimensional array
+    """
     arr_3d = []
     temp_2d = []
-    for i in range(1,len(array)):
+    for i in range(1, len(array)):
         temp_2d.append(array[i])
         if i % num_timestamps == 0:
             arr_3d.append(temp_2d)
             temp_2d = []
-    
     return arr_3d
 
-def create_2d_y_array(array, num_timestamps):
-    arr_2d_y_train = []
+
+def create_2d_y_array(array: list, num_timestamps: int):
+    """Generate two-dimensional array for RNN
+
+    Args:
+        array ([float]): Data from sensors to be concatted
+        num_timestamps (int): Number of timestamps to concat into new array
+
+    Returns:
+        [[float]]: Two-dimensional array
+    """
     temp_y_train = []
     mode_arr = []
 
-    for i in range(1,len(array)):
-        #temp_2d.append(array[0])
+    for i in range(1, len(array)):
+        # temp_2d.append(array[0])
         temp_y_train.append(array[i])
         if i % num_timestamps == 0:
             mode_arr.append(temp_y_train)
@@ -34,11 +50,11 @@ def create_2d_y_array(array, num_timestamps):
         mode = stats.mode(mode_arr[i])
         y_train_to_be_encoded.append(mode.mode[0])
 
-    #ONE HOT ENCODING
+    # ONE HOT ENCODING
     encoding = []
     for value in y_train_to_be_encoded:
         vector = [0 for _ in range(9)]
         vector[value] = 1
         encoding.append(vector)
-    
+
     return np.array(encoding)
