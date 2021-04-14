09c9797d7f7c5c53fb1000d67a98e99b62a598c6
simonaardal <simon.aardal@gmail.com>
2021-04-14 16:09:25 +0200

    ml

--- model/utils/declarations.py
+++ model/utils/declarations.py
@@ -61,4 +61,6 @@ testing_files = {
                          "../../data/annotation/testing/008_test_3_030321.txt"),
     "009": testing_class("../../data/test_data/009_test_3_120421.csv",
                           "../../data/annotation/testing/009_test_3_120421.txt"),
+    "010": testing_class("../../data/test_data/010_test_3_140421.csv",
+                          "../../data/annotation/testing/010_test_3_140421.txt"),
 }
