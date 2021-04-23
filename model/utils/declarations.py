27574edc93ae20e06b74ae31565bb280130c3369
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2021-04-23 12:03:13 +0000

    Fix dec

--- model/utils/declarations.py
+++ model/utils/declarations.py
@@ -59,8 +59,8 @@ testing_files = {
                         "../../data/annotation/testing/007_test_3_030321.txt"),
     "009": testing_class("../../data/test_data/009_test_3_120421.csv",
                           "../../data/annotation/testing/009_test_3_120421.txt"),
-    "010": testing_class("../../data/train_data/010_test_3_220421.csv",
-                        "../../data/annotation/training/010_test_3_220421.txt"),
+    "010": testing_class("../../data/test_data/010_test_3_220421.csv",
+                        "../../data/annotation/testing/010_test_3_220421.txt"),
 }
 
 
