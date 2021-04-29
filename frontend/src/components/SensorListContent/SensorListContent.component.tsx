8055b7dd55ae2f1b76afb4c69af3a291df1f6845
Ole Jonas Liahagen <jonas.liahagen@hotmail.com>
2021-04-30 00:28:42 +0200

    Small qol changes

--- frontend/src/components/SensorListContent/SensorListContent.component.tsx
+++ frontend/src/components/SensorListContent/SensorListContent.component.tsx
@@ -112,7 +112,7 @@ export const SensorListContent: React.FC<ListProps> = (props) => {
 	 *
 	 * @param sensor An object of the sensor type
 	 */
-	const addSensors = (sensor: Sensor) => {
+	const addSensor = (sensor: Sensor) => {
 		let helper = sensors;
 		helper.push(sensor);
 		setSensors(helper);
@@ -215,7 +215,7 @@ export const SensorListContent: React.FC<ListProps> = (props) => {
 					</Button>
 					{open ? (
 						<SensorModal
-							sendSensors={addSensors}
+							sendSensors={addSensor}
 							alreadyConnected={getSensorsConnectedNames()}
 							close={closeModal}
 							open={open}
